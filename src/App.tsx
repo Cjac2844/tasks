630f37efe3257657543c8298499c1a986450ca7f
Connor Jackson <cjac@udel.edu>
2024-02-15 14:27:20 -0500

    Finished solved-html-css branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./App.css";
+import image1 from "./images/Big-Chungus.jpg";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App(): JSX.Element {
     return (
@@ -7,11 +9,43 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript by Connor Jackson
             </header>
+            <h1> Big Chungus, Big Chungus, Big Chungus </h1>
+            <img src={image1} alt="Big Chungus the movie" />
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. I love Big Chungus and he is a very big
                 bunny!
             </p>
+            <ol>
+                <li>Big</li>
+                <li>Chungus</li>
+                <li>Is a big bunny</li>
+            </ol>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
+            <Container>
+                <Row>
+                    <Col>
+                        <div
+                            style={{
+                                width: "100px",
+                                height: "100px",
+                                backgroundColor: "#ff0000"
+                            }}
+                        ></div>
+                    </Col>
+                    <Col>
+                        <div
+                            style={{
+                                width: "100px",
+                                height: "100px",
+                                backgroundColor: "#ff0000"
+                            }}
+                        ></div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
